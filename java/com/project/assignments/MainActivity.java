a2e7b806c1e4e9573ef4267dc181b31efd7d2061
tewelve <kashyaprishi4u@yahoo.com>
2017-09-18 14:58:27 +0530

    Initial commit

---
+++ java/com/project/assignments/MainActivity.java
@@ -0,0 +1,74 @@
+package com.project.assignments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements Animation.AnimationListener {
+
+    ImageView imageView;
+    Button btnStart;
+    Animation animFadein, animFadeout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        btnStart = (Button) findViewById(R.id.btnStart);
+        // loads the animation
+        animFadeout= AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fadeout);
+        animFadein = AnimationUtils.loadAnimation(getApplicationContext(),
+                R.anim.fade_in);
+
+        // set listener
+        animFadein.setAnimationListener(this);
+
+        // button click event
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            //fadein fade out effect in image view
+            @Override
+            public void onClick(View v) {
+                imageView.setVisibility(View.VISIBLE);
+
+                // start the animation
+                imageView.startAnimation(animFadein);
+            }
+        });
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+        // check for fade in animation with image
+        if (animation == animFadein) {
+            imageView.startAnimation(animFadeout);
+            animFadeout.start();
+        }
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
